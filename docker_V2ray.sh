8869d95fd58afbef33951fc0ce84d52e6658f367
GZ <55527553+GZ1903@users.noreply.github.com>
2022-05-16 13:34:50 +0800

    Update docker_V2ray.sh

--- docker_V2ray.sh
+++ docker_V2ray.sh
@@ -1,68 +1,114 @@
-#!/bin/sh
-#
-#Date:2021.12.21
-#Author:GZ
-#Mail:V2board@qq.com
+#!/bin/bash
 
-process()
-{
-install_date="XrayR_LuFly_$(date +%Y-%m-%d_%H:%M:%S).log"
-printf "
-\033[36m#######################################################################
-#                     欢迎使用XrayR一键对接脚本                       #
-#                脚本适配环境CentOS7+/RetHot7+、内存1G+               #
-#                更多信息请访问 https://gz1903.github.io              #
-#######################################################################\033[0m
-"
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
 
-echo -e "\033[36m#######################################################################\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#                  正在安装基础组件  请稍等~                          #\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#######################################################################\033[0m"
-yum -y install wget curl
-yum -y install ca-certificates
+cur_dir=$(pwd)
 
-echo -e "\033[36m#######################################################################\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#                  正在同步时间  请稍等~                              #\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#######################################################################\033[0m"
-yum -y install ntpdate
-timedatectl set-timezone Asia/Shanghai
-ntpdate ntp1.aliyun.com
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
-echo -e "\033[36m#######################################################################\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#                  正在配置防火墙  请稍等~                            #\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#######################################################################\033[0m"
-systemctl disable firewalld
-systemctl stop firewalld
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+fi
 
-echo -e "\033[36m#######################################################################\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#                  正在安装docker  时间较长请稍等~                    #\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#######################################################################\033[0m"
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io -y
-systemctl start docker
-systemctl enable docker
-# 安装Docker-compose
-curl -fsSL https://get.docker.com | bash -s docker
-curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+echo "系统：${release}"
 
-echo -e "\033[36m#######################################################################\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#                  正在部署docker  时间较长请稍等~                    #\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#######################################################################\033[0m"
-yum install -y git
+arch=$(arch)
+
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+  arch="amd64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+  arch="arm64"
+else
+  arch="amd64"
+  echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
+fi
+
+echo "架构: ${arch}"
+
+if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
+    echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
+    exit 2
+fi
+
+os_version=""
+
+# os version
+if [[ -f /etc/os-release ]]; then
+    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
+    fi
+fi
+
+echo "版本：${os_version}"
+
+install_base() {
+    if [[ x"${release}" == x"centos" ]]; then
+        yum -y install wget curl ca-certificates ntpdate
+	timedatectl set-timezone Asia/Shanghai
+	ntpdate ntp1.aliyun.com
+	systemctl disable firewalld
+	systemctl stop firewalld
+	yum install -y yum-utils
+	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	yum install docker-ce docker-ce-cli containerd.io -y
+	systemctl start docker
+	systemctl enable docker
+	curl -fsSL https://get.docker.com | bash -s docker
+	curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
+    else
+        systemctl disable firewalld
+        systemctl stop firewalld
+        sudo apt-get update
+	sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+	systemctl start docker
+	systemctl enable docker
+	curl -fsSL https://get.docker.com | bash -s docker
+	curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
+    fi
+}
+
+install_xrary() {
 rm -rf /usr/local/src/XrayR-release
-git clone https://github.com/XrayR-project/XrayR-release /usr/local/src/XrayR-release
+git clone https://github.com/GZ1903/XrayR-release.git /usr/local/src/XrayR-release
 cd /usr/local/src/XrayR-release
 wget --no-check-certificate https://raw.githubusercontent.com/supermalio/docker/main/rico-docker.sh && chmod +x  rico-docker.sh && bash rico-docker.sh
 
@@ -209,46 +255,13 @@ Nodes:
   #         ALICLOUD_ACCESS_KEY: aaa
   #         ALICLOUD_SECRET_KEY: bbb
 eof
-
-
-echo -e "\033[36m#######################################################################\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#                  正在启动  请稍等~                                  #\033[0m"
-echo -e "\033[36m#                                                                     #\033[0m"
-echo -e "\033[36m#######################################################################\033[0m"
-
-
 cp -i /usr/local/bin/docker-compose /usr/bin/
-
 cd /usr/local/src/XrayR-release/config 
-
 docker-compose up -d
-
-echo $?="对接已完成"
-
+echo -e "${green}Docker_xrayr${plain} 安装完成，已设置开机自启"
 # 清除缓存垃圾
-# rm -rf /usr/local/src/XrayR-release
 rm -rf /usr/local/src/docker_V2ray
-
-echo -e "\033[32m--------------------------- 已完成 ---------------------------\033[0m"
-echo -e "\033[32m 文件目录     :/usr/local/src/XrayR-release\033[0m"
-echo -e "\033[32m 安装日志文件 :/var/log/"$install_date
-echo -e "\033[32m------------------------------------------------------------------\033[0m"
-echo -e "\033[32m 如果安装有问题请反馈安装日志文件。\033[0m"
-echo -e "\033[32m 使用有问题请在这里寻求帮助:https://gz1903.github.io\033[0m"
-echo -e "\033[32m 电子邮箱:v2board@qq.com\033[0m"
-echo -e "\033[32m------------------------------------------------------------------\033[0m"
 }
-LOGFILE=/var/log/"XrayR_LuFly_$(date +%Y-%m-%d_%H:%M:%S).log"
-touch $LOGFILE
-tail -f $LOGFILE &
-pid=$!
-exec 3>&1
-exec 4>&2
-exec &>$LOGFILE
-process
-ret=$?
-exec 1>&3 3>&-
-exec 2>&4 4>&-\
-
-
+echo -e "${green}开始安装${plain}"
+install_base
+install_xrary
